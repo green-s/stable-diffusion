9b61abcf23643491749e59ab69685064cac30010
Sam Green <sam.green81@gmail.com>
2022-08-27 10:43:40 +0100

    Streamlit: Format

--- scripts/streamlit/txt2img.py
+++ scripts/streamlit/txt2img.py
@@ -138,17 +138,26 @@ def check_prompt_length(model, prompt):
     tokenizer = model.cond_stage_model.tokenizer
     max_length = model.cond_stage_model.max_length
 
-    info = model.cond_stage_model.tokenizer([prompt], truncation=True, max_length=max_length, return_overflowing_tokens=True, padding="max_length", return_tensors="pt")
-    ovf = info['overflowing_tokens'][0]
+    info = model.cond_stage_model.tokenizer(
+        [prompt],
+        truncation=True,
+        max_length=max_length,
+        return_overflowing_tokens=True,
+        padding="max_length",
+        return_tensors="pt",
+    )
+    ovf = info["overflowing_tokens"][0]
     overflowing_count = ovf.shape[0]
     if overflowing_count == 0:
         return
 
     vocab = {v: k for k, v in tokenizer.get_vocab().items()}
     overflowing_words = [vocab.get(int(x), "") for x in ovf]
-    overflowing_text = tokenizer.convert_tokens_to_string(''.join(overflowing_words))
+    overflowing_text = tokenizer.convert_tokens_to_string("".join(overflowing_words))
 
-    st.warning(f"Too many input tokens. ({len(overflowing_words)}) have been truncated: {overflowing_text}")
+    st.warning(
+        f"Too many input tokens. ({len(overflowing_words)}) have been truncated: {overflowing_text}"
+    )
 
 
 st.set_page_config(
