d4d897161403f9d0b68c728e27aa6cc8a3608fba
Sam Green <sam.green81@gmail.com>
2022-09-11 01:18:01 +0100

    Streamlit: Improve normalize_latent, better sat

--- ldm/models/diffusion/normalize_latent.py
+++ ldm/models/diffusion/normalize_latent.py
@@ -4,9 +4,11 @@ import torch
 def normalize_latent(x, max_val=1.7, quantile_val=0.975):
     x = x.detach().clone()
     for i in range(x.shape[0]):
-        if x[[i], :].std() > 1.0:
-            x[[i], :] = x[[i], :] / x[[i], :].std()
-        s = torch.quantile(torch.abs(x[[i], :]), quantile_val)
-        s = torch.maximum(s, torch.ones_like(s) * max_val)
-        x[[i], :] = x[[i], :] / (s / max_val)
+        for chl in range(4):
+            channel_std = x[i, chl, :, :].std()
+            if channel_std > 1.0:
+                x[i, chl, :, :] = x[i, chl, :, :] / channel_std
+            s = torch.quantile(torch.abs(x[i, chl, :, :]), quantile_val)
+            s = torch.maximum(s, torch.ones_like(s) * max_val)
+            x[i, chl, :, :] = x[i, chl, :, :] / (s / max_val)
     return x
